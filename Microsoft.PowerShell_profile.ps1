d043b9ff5310d47e6d1d38bb261cb9399928c6e9
JJ Bussert <16327051+JJBussert@users.noreply.github.com>
2024-10-10 14:56:36 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,5 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Import-Module AzureAD
-Set-Theme Paradox
-
+oh-my-posh --init --shell pwsh --config c:\_\_terminal\e13.json | Invoke-Expression
+Import-Module -Name Terminal-Icons
 $d = Get-Date
 if($d.ToString().EndsWith("AM")) {
     Write-Host Good Morning JJ
